90fac36e45a1170170b940821f9b38c980469258
zebrapgi <52920632+zebrapgi@users.noreply.github.com>
2026-09-18 21:52:07 +0200

    app_gattile.py

--- app_gattile.py
+++ app_gattile.py
@@ -4,6 +4,8 @@ import os
 import pandas as pd
 import pytz
 import streamlit as st
+import firebase_admin
+from firebase_admin import credentials, firestore
 
 st.set_page_config(
     page_title="Gestione Turni Gattile", page_icon="🐱", layout="wide"
